8a32b5b371f225403b86652990222ea217f5b1ed
mohamed2m2018 <33550035+mohamed2m2018@users.noreply.github.com>
2018-10-28 19:44:12 +0200

    Editing background and foreground colors

--- scanner.py
+++ scanner.py
@@ -14,9 +14,10 @@ window.wm_state('zoomed')
 
 #asking him for entering the code
 
-label=ttk.Label(text='Please enter code in tiny language',font='Helvetica 14 bold')
+label=Label(text='Please enter code in tiny language',font='Helvetica 20 bold',fg='black',
+                  )
 
-label.grid(column=1,row=0,pady=10)
+label.grid(column=1,row=0,pady=13)
 
 #scroll bar
 
@@ -26,7 +27,7 @@ scrollbar.grid(column=2,row=1,sticky=N+S+W)
 
 #textbox input field
 
-input=Text(master=window,width=50,bg="#FFF8DC",insertbackground="green",
+input=Text(master=window,width=60,bg="#FFF8DC",insertbackground="green",
            fg="green",font='Helvetica 14 bold',yscrollcommand=scrollbar.set)
 
 scrollbar.configure(command=input.yview)
@@ -37,9 +38,7 @@ input.grid(column=1,row=1,padx=(20,0),pady=20)
 
 #button to submit code supplied by the command to be executed
 
-button=ttk.Button (master=window,text='Submit code',command=lambda :getCode(input))
+button=Button (master=window,text='Submit code',command=lambda :getCode(input),bg='#107896',fg='#FDFBFB',font="Helvetica",width=67,padx=16)
 button.grid(column=1,row=2,pady=7)
-
-
 
 window.mainloop()
