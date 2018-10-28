cf0bf54c7cf8c966388b5cb130cf6ccc7f4eed65
mohamed2m2018 <33550035+mohamed2m2018@users.noreply.github.com>
2018-10-28 21:44:11 +0200

    Improving label styling

--- getCode.py
+++ getCode.py
@@ -2,8 +2,16 @@ import tkinter as tk
 from parseCode import parseCode
 from createSpace import createSpace
 def getCode(input):
-    label=tk.Label(text='your code has been added successfully')
-    label.grid(column=1,row=3)
+    label=tk.Label(text='\n \n Congratulations our dear user :D \n'
+                        'The result is added successfully to '
+                        'csv file \n in the same directory of this exe \n \n',
+                   font='Helvetica 14 bold',
+                   fg='#FFFFFF',
+                   bg='#EA5E3D',
+                   padx=100
+                   )
+    label.grid(column=3,row=1)
     code=str(input.get("1.0","end-1c"))
     code=createSpace(code)
     parseCode(code)
+
