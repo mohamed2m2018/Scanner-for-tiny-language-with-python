ed998e9668a8fdb534c0f3b7200e5bd69830a406
mohamed2m2018 <33550035+mohamed2m2018@users.noreply.github.com>
2018-10-28 21:43:06 +0200

    Print output to csv file

--- parseCode.py
+++ parseCode.py
@@ -4,20 +4,33 @@ from isNumber import isNumber
 from isSpecialSymbol import isSpecialSymbol
 from isSpecialSymbol import checkSymbol
 import re
+import csv
+import os.path
 def parseCode(code):
-    code=re.sub(r'\{[^{}]*\}', '', code)
-    words=code.split()
-    for word in words:
-        if isReservedWord(word):
-            print (word , "is reserved word")
-        elif isSpecialSymbol(word):
-            wordLabel=checkSymbol(word)
-            if word == "α":
-                word=":="
-            if word=="μ":
-                word="*"
-            print (word, "is special symbol of the type",wordLabel)
-        elif isNumber (word):
-            print (word, "is a number")
-        else:
-            print(word ,"is identifier")
+    var=''
+    if os.path.exists('Scanner_result.csv'):
+        var='w'
+    else:
+        var='x'
+    with open('Scanner_result.csv', mode=var) as Scanner_result:
+        fieldnames = ['Token','Token type']
+        writer = csv.DictWriter(Scanner_result, fieldnames=fieldnames)
+        writer.writeheader()
+        code = re.sub(r'\{[^{}]*\}', '', code)
+        words = code.split()
+        for word in words:
+            if isReservedWord(word):
+                writer.writerow({'Token': word, 'Token type': 'Reserved word'})
+
+            elif isSpecialSymbol(word):
+                wordLabel=checkSymbol(word)
+                if word == "α":
+                    word=":="
+                if word=="μ":
+                    word="*"
+                writer.writerow({'Token': word, 'Token type': 'special symbol of type {}'.format(wordLabel)})
+
+            elif isNumber (word):
+                writer.writerow({'Token': word, 'Token type': 'Number'})
+            else:
+                writer.writerow({'Token': word, 'Token type': 'Identifier'})
